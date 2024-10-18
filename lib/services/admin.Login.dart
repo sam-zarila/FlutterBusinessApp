2f16cf252c004461be858c8980782294bb07e12d
zarila00 <shyreenkhoza8@gmail.com>
2024-10-17 18:10:18 -0700

    login

--- lib/services/admin.Login.dart
+++ lib/services/admin.Login.dart
@@ -3,9 +3,22 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/adminModel.dart';
 
-
-class AuthService{
+class AuthService {
   final String baseUrl = 'http://10.0.2.2:3000/admin';
 
-  Future<AuthResponse?>
+  Future<AuthResponse?> login(Adminmodel user) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/login'),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(user.toJson()),
+      );
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+         final 
+      } else {}
+    } catch (e) {}
+  }
 }
