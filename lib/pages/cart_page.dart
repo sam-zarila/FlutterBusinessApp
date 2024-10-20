4294c5b4dc5ede81ce624951077b8fd8021e2727
sam zarila <zarilatroxx99@gmail.com>
2024-10-21 00:06:46 +0100

    cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -19,7 +19,7 @@ class CartPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: FutureBuilder<List<CartModel>>(
-          future: cartService.fetchCartModel(),
+          future: cartService.fetchCartItems(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(
@@ -35,31 +35,48 @@ class CartPage extends StatelessWidget {
               final CartModel = snapshot.data!;
 
               double total = 0.0;
-              for( var item in CartModel){
-                 total += double.parse(item.price.replaceAll(RegExp(r'[^0-9.]'), '')) * item.quantity;
-
+              for (var item in CartModel) {
+                total += double.parse(
+                        item.price.replaceAll(RegExp(r'[^0-9.]'), '')) *
+                    item.quantity;
               }
               return Column(
                 children: [
                   Expanded(
-                    child:ListView.builder(
-                      itemBuilder:(context, index){
-                        return Card(
-                          margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
-                          child: ListTile(
-                             leading: Image.network(CartModel[index].image, width: 50, height: 50, fit: BoxFit.cover),
-                              title: Text(CartModel[index].name), // Display name
-                          subtitle: Text('Quantity: ${CartModel[index].quantity}\nPrice: ${CartModel[index].price}'), // Display quantity and price
-                          trailing: Text('Total: \$${(double.parse(CartModel[index].price.replaceAll(RegExp(r'[^0-9.]'), '')) * CartModel[index].quantity).toStringAsFixed(2)}'),
-
-                          ),
-
-                        );
-                      }) )
+                      child: ListView.builder(itemBuilder: (context, index) {
+                    return Card(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 10.0),
+                      child: ListTile(
+                        leading: Image.network(CartModel[index].image,
+                            width: 50, height: 50, fit: BoxFit.cover),
+                        title: Text(CartModel[index].name), // Display name
+                        subtitle: Text(
+                            'Quantity: ${CartModel[index].quantity}\nPrice: ${CartModel[index].price}'), // Display quantity and price
+                        trailing: Text(
+                            'Total: \$${(double.parse(CartModel[index].price.replaceAll(RegExp(r'[^0-9.]'), '')) * CartModel[index].quantity).toStringAsFixed(2)}'),
+                      ),
+                    );
+                  }
+                  
+                  
+                  )),
+                   Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      
+                    ) ,
+                    
+                    )
                 ],
-              )
+              );
+              
             }
-          }),
+           
+          }
+          
+          
+          ),
     );
   }
 }
