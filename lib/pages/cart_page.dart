4f103982869a97bb118c49538cee373c7f49e54d
sam zarila <zarilatroxx99@gmail.com>
2024-10-20 23:21:24 +0100

    cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,3 +1,12 @@
 import '../models/cart_model.dart';
 import '../services/cart_service.dart';
 import 'package:flutter/material.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
