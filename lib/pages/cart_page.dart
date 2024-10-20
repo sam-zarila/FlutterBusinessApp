a63cebb480e87d303b6f22aa956afb46946203c7
sam zarila <zarilatroxx99@gmail.com>
2024-10-20 23:57:38 +0100

    cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -19,7 +19,7 @@ class CartPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: FutureBuilder<List<CartModel>>(
-          future: cartService.fetchCartItems(),
+          future: cartService.fetchCartModel(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(
@@ -39,6 +39,25 @@ class CartPage extends StatelessWidget {
                  total += double.parse(item.price.replaceAll(RegExp(r'[^0-9.]'), '')) * item.quantity;
 
               }
+              return Column(
+                children: [
+                  Expanded(
+                    child:ListView.builder(
+                      itemBuilder:(context, index){
+                        return Card(
+                          margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+                          child: ListTile(
+                             leading: Image.network(CartModel[index].image, width: 50, height: 50, fit: BoxFit.cover),
+                              title: Text(CartModel[index].name), // Display name
+                          subtitle: Text('Quantity: ${CartModel[index].quantity}\nPrice: ${CartModel[index].price}'), // Display quantity and price
+                          trailing: Text('Total: \$${(double.parse(CartModel[index].price.replaceAll(RegExp(r'[^0-9.]'), '')) * CartModel[index].quantity).toStringAsFixed(2)}'),
+
+                          ),
+
+                        );
+                      }) )
+                ],
+              )
             }
           }),
     );
