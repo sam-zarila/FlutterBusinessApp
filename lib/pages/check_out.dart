30cee151f977e96eee5e2aacf3b8be61fb92fb7d
sam zarila <zarilatroxx99@gmail.com>
2024-10-23 19:35:53 +0200

    commit

--- lib/pages/check_out.dart
+++ lib/pages/check_out.dart
@@ -1,2 +1,10 @@
 import '../models/cart_model.dart';
 import 'package:flutter/material.dart';
+class CheckoutPage extends StatelessWidget {
+  const CheckoutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
