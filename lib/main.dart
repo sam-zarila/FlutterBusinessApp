44e287b8fc4a47f39461c2eac2cad1a65542ea57
sam zarila <zarilatroxx99@gmail.com>
2024-10-25 00:46:56 +0200

    user register

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
-
+import 'theme.dart';
+import './pages/LoginPage.dart';
+import './pages/signUp.dart';
 // import './pages/bottomNavBar.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
+    theme: AppTheme.theme,
+    initialRoute: '/',
+    routes: {
+      '/': (context) => LoginPage(),
+      '/signup': (context) => SignUpPage(),
+      // '/bottomnavbar': (context) => BottomNavbar(),
+    },
     
    
     // home:BottomNavbar (),
